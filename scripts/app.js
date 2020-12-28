bca09a43cae40355a4636b6df88f1a9c46c9d244
jessicalh <jessicalynnehansen@gmail.com>
2020-12-28 11:48:44 -0700

    Initial commit - start of project planning - basic assets gathered, basic html and styling, pseudo code.

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+console.log('Project Zero');
+/*              Project Zero: projectZora
+                Pseudo code MVP rough draft
+
+1. Create a Tamagotchi class - cat
+    - Name - default Zora
+    - Hunger: 0
+    - Sleepiness: 0
+    - Boredom: 0
+    - Age: 0
+
+2. Create a stats display and assign methods
+    - Name 
+    - Hunger: 0 --> 10 - increment by 1 every 10 seconds 
+        - possibly add increment by 1 after 'playing' with cat.
+
+    - Sleepiness: 0 --> 10 - increment every 10 seconds 
+        - possibly add increment by 1 after 'playing' with cat.
+
+    - Boredom: 0 --> 10 - increment by 1 every 10 seconds
+       - possibly add increment by 1 after making cat 'sleep'. 
+
+    - Age: 0 --> 10 - increment by 1 every 60 seconds. 
+
+
+3. Create button bar and assign methods (icons in /assets/)
+    - Feed --> resets hunger to 0 - animate cat happy icon
+        - possibly animate fish on screen
+
+    - Play --> resets boredom to 0 - animate cat sitting icon
+        - possibly animate ball of yarn on screen
+
+    - Sleep --> resets sleepiness to 0 - turn screen black with cat sleeping icon in middle of screen
+        - possibly animate 'zzz'
+
+4. Add start screen - click any button to start.
+    - cat standing icon animation
+    - possibly include 'adoption' dialogue. 
+
+5. GAME OVER - when any stat hits 10
+    - Zora runs away, animiate angry cat icon
+    - except age, cat sleeping icon animates on screen instead. 
+        
+*/
