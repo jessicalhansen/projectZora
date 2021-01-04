9e98c0fa271ea4c004edcbd22702c658c24a963a
jessicalh <jessicalynnehansen@gmail.com>
2021-01-04 10:25:31 -0700

    Commit 25 - Final commit for presentation. Edited links to hopefully be deployable.

--- scripts/app.js
+++ scripts/app.js
@@ -1,54 +1,6 @@
-console.log('Project Zero');
-/*              Project Zero: projectZora
-                Pseudo code MVP rough draft
-
-1. Create a Tamagotchi class - cat
-    - Name - default Zora
-    - Hunger: 0
-    - Sleepiness: 0
-    - Boredom: 0
-    - Age: 0
-
-2. Create a stats display and assign methods
-    - Timer 
-    - Hunger: 0 --> 10 - increment by 1 every 10 seconds 
-        - possibly add increment by 1 after 'playing' with cat.
-
-    - Sleepiness: 0 --> 10 - increment every 10 seconds 
-        - possibly add increment by 1 after 'feeding' cat.
-
-    - Boredom: 0 --> 10 - increment by 1 every 10 seconds
-       - possibly add increment by 1 after making cat 'sleep'. 
-
-    - Age: 0 - possibly increment by 1 every 30 seconds. 
-
-
-3. Create button bar and assign methods (icons in /assets/)
-    - Feed --> resets hunger to 0 - animate cat happy icon
-        - possibly animate fish on screen
-
-    - Play --> resets boredom to 0 - animate cat sitting icon
-        - possibly animate ball of yarn on screen
-
-    - Sleep --> resets sleepiness to 0 - turn screen black with cat sleeping icon in middle of screen
-        - possibly animate 'zzz'
-
-4. Add start screen - click any button to start.
-    - cat standing icon animation
-    - possibly include 'adoption' dialogue. 
-
-5. GAME OVER - when any stat hits 10
-    - Zora runs away, animiate angry cat icon
-    - except age.
-        
-*/
-
-
-
 ///////////////////////////////////////////////////
              //   Game Objects   //
 ///////////////////////////////////////////////////
-
 class Cat {
     constructor(name=`Zora`) {
         this.name = name;
@@ -66,10 +18,10 @@ const newCat = new Cat;
         //   Sound Effects   //
 ///////////////////////////////////////////////
 
-const shortMeow = new Audio(`/assets/cat-meow-short.wav`);
-const excitedPurr = new Audio(`/assets/excitedpurr.wav`);
-const pouringCatFood = new Audio(`/assets/pouring-catfood.wav`);
-const catGrowls = new Audio(`/assets/catgrowls.wav`);
+const shortMeow = new Audio(`./assets/cat-meow-short.wav`);
+const excitedPurr = new Audio(`./assets/excitedpurr.wav`);
+const pouringCatFood = new Audio(`./assets/pouring-catfood.wav`);
+const catGrowls = new Audio(`./assets/catgrowls.wav`);
 
 ///////////////////////////////////////////////
            //   Stat functions   //
